d6d6e978ba4a60486d2a7e66a5b4c96367ac967c
shocknik <teilor2010@gmail.com>
2023-11-18 18:04:51 +0300

    "Add logging"

--- connect_to_db.py
+++ connect_to_db.py
@@ -1,25 +1,28 @@
+import logging_config
+import logging
 from peewee import *
+
+logger = logging.getLogger(__name__)
+logger.info("An info")
+
 
 db = SqliteDatabase('D:\\My_projects\\Protoсols\\tests.db')
 
 class BaseModel(Model):    
     class Meta:
         database = db
-        
+
+
+class TestCategory(BaseModel):
+    ID = PrimaryKeyField(null=False)
+    category = CharField(unique=True)
+    
         
 class PMITests(BaseModel):
     ID = PrimaryKeyField(null=False)
     name = CharField(unique=True)
-    
-    
-class FormForProtocols(BaseModel):
-    ID = PrimaryKeyField(null=False)
     form = CharField(unique=True)
-    
-    
-class TestCategory(BaseModel):
-    ID = PrimaryKeyField(null=False)
-    category = CharField(unique=True)
+    id_category=ForeignKeyField(TestCategory)
     
     
 class TestCriteria(BaseModel):
@@ -33,5 +36,9 @@ class RelationshipCategoryCriteria(BaseModel):
     
     
 def create_tables():
-    with db:
-        db.create_tables([PMITests, FormForProtocols, TestCategory, TestCriteria, RelationshipCategoryCriteria])
+    try:
+        with db:
+            db.create_tables([TestCategory, PMITests, TestCriteria, RelationshipCategoryCriteria])
+    except Exception as e:
+        logger.error(f'Ошибка в создании таблицы: {e}', exc_info=True)
+create_tables()
---
+++ logging_config.py
@@ -0,0 +1,24 @@
+import logging
+import logging.config
+from pythonjsonlogger import jsonlogger
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "json": {
+            "format": "%(asctime)s %(levelname)s %(message)s %(module)s",
+            "class": "pythonjsonlogger.jsonlogger.JsonFormatter",
+        }
+    },
+    "handlers": {
+        "stdout": {
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "formatter": "json",
+        }
+    },
+    "loggers": {"": {"handlers": ["stdout"], "level": "DEBUG"}},
+}
+
+logging.config.dictConfig(LOGGING)
